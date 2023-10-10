19463a97f376174577b2e6192ca407d4ce364c72
iMonie <buildempire.uk@gmail.com>
2023-10-09 19:03:31 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+
+char c;
+
+c = 'z';
+
+while
+(c >= 'z') {
+putchar(c);
+c--;
+}
+putchar('\n');
+
+return (0);
+
+}
+
+
+
