6ba39132ba504da70e8dbd2a8665a914241ea6cb
iMonie <buildempire.uk@gmail.com>
2023-10-09 18:40:40 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - 6_print_numberz
+ * Return: Always 0
+ */
+int main(void)
+{
+int c;
+
+c = 0;
+while
+(c < 10) {
+putchar(c + '0');
+c++;
+}
+putchar('\n');
+
+return (0);
+
+}
